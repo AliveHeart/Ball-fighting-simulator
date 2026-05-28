d4e1029da7febb3b86eea669df43f316a6c08857
AliveHeart <mirarafathali7@gmail.com>
2026-05-28 10:08:13 +0530

    Global variable 'gobal_speed' added

--- config.py
+++ config.py
@@ -1,16 +1,20 @@
 players = []
 
+NamesDOT = ["fire", "bfire", "poison", "virus", "frost", "static", "leech"]
+
 Abilities = {"fire" : {"dmg": 0.7, "type": "burn", "tick": 1, "dura" : 7},
         "bfire" : {"dmg": 2, "type": "burn", "tick": 0.5, "dura": 7},
-        "poison" : {"dmg": 0.6, "type": "poison", "tick": 1, "dura": 10},
-        "virus" : {"dmg": 0.1, "type": "chain", "tick": 1, "dura": 20},
-        "frost" : {"dmg": 0.5, "type": "freeze", "tick": 0.5, "dura": 6},
-        "static" : {"dmg": 1, "type": "chain", "tick": 0.5, "dura": 3},
-        "leech" : {"dmg": 0.6, "type": "leech", "tick": 0.5, "dura": 1},
+        "poison" : {"dmg": 0.6, "type": "poison", "tick": 1, "dura": 7},
+        "virus" : {"dmg": 0.1, "type": "chain", "tick": 1, "dura": 15},
+        "frost" : {"dmg": 0.5, "type": "freeze", "tick": 0.5, "dura": 4},
+        "static" : {"dmg": 1, "type": "chain", "tick": 1, "dura": 2},
+        "leech" : {"dmg": 0.4, "type": "leech", "tick": 0.25, "dura": 1},
 }
 
 BASE_WIDTH, BASE_HEIGHT = 1280, 720
 SCREEN_WIDTH, SCREEN_HEIGHT = 1280, 720
+
+global_speed = 1
 
 SCREENX = (SCREEN_WIDTH / BASE_WIDTH)
 SCREENY = (SCREEN_HEIGHT / BASE_HEIGHT)
