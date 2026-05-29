01069bb37779efdbb0cebe008e1945405ada172b
AliveHeart <mirarafathali7@gmail.com>
2026-05-29 14:26:45 +0530

    Optimized collision system for better performance

--- Main.py
+++ Main.py
@@ -45,7 +45,10 @@ frost_Image = pygame.image.load(os.path.join(base_dir, 'Assets', 'images', 'fros
 leech_Image = pygame.image.load(os.path.join(base_dir, 'Assets', 'images', 'leech.png')).convert_alpha()
 
 tick = 0
+gameTick = 0
 dt = 0
+Frame = 0
+FPS = 0
 
 bigFont = pygame.font.Font(None, int(72 * SCREENX))
 Font = pygame.font.Font(None, int(52 * SCREENX))
@@ -106,8 +109,8 @@ class Player:
 
         self.text_surface = self.font.render(displayString, True, WHITE)
 
-        pygame.draw.circle(screen, (255, 255, 255), (self.x, self.y), self.r + 5)
-        pygame.draw.circle(screen, self.clr, (self.x, self.y), self.r)
+        pygame.draw.circle(screen, (255, 255, 255), (self.x, self.y), self.r)
+        pygame.draw.circle(screen, self.clr, (self.x, self.y), self.r - 5 * SCREENY)
         screen.blit(self.text_surface, (self.x - self.r/2, self.y))
 
     def move(self, dt):
@@ -140,22 +143,115 @@ class Player:
             if (self.armr > 0):
                 self.armr -= 1
             else:
-                self.hp -=1
+                self.hp -= 1
 
 def initiateBattle():
-    players.append(Player(100, 100, 80, 500, 100, 5, (255,0,0), 500, "fire", 1, "none"))  # red ball
-    players.append(Player(300, 200, 80, 500, 100, 5, (0,255,0), 500, "poison", 1, "none"))  # green ball
-    players.append(Player(500, 400, 80, 500, 100, 5, (10,10,10), 500, "bfire", 1, "none"))  # black ball
-    players.append(Player(500, 600, 80, 500, 100, 5, (0,255,255), 500, "frost", 1, "none"))  # cyan  ball
-    players.append(Player(300, 600, 80, 500, 100, 5, (0,100,255), 500, "static", 1, "none")) # blue ball
-    players.append(Player(300, 300, 80, 500, 100, 5, (1, 50, 32), 500, "virus", 1, "none"))  # dark green ball
-    players.append(Player(200, 500, 80, 500, 100, 5, (50, 10, 32), 500, "leech", 1, "none"))  # dark red ball
-    #players.append(Player(500, 500, 80, 500, 100, 5, (125, 125, 125), 800, "mage", 1, "none"))
+    #--------------------(X position, Y postion, radius, health, armor, damage, colour, speed, effect, tier, ultimate)
+    # same colour balls won't damage eachother
+    players.append(Player(100, 100, 50, 500, 100, 5, (255,0,0), 500, "fire", 1, "none"))  # red ball
+    players.append(Player(300, 200, 50, 500, 100, 5, (0,255,0), 500, "poison", 1, "none"))  # green ball
+    players.append(Player(500, 400, 50, 500, 100, 5, (10,10,10), 500, "bfire", 1, "none"))  # black ball
+    players.append(Player(500, 600, 50, 500, 100, 5, (0,255,255), 500, "frost", 1, "none"))  # cyan  ball
+    players.append(Player(300, 600, 50, 500, 100, 5, (0,100,255), 500, "static", 1, "none")) # blue ball
+    players.append(Player(300, 300, 50, 500, 100, 5, (1, 50, 32), 500, "virus", 1, "none"))  # dark green ball
+    players.append(Player(200, 500, 50, 500, 100, 5, (50, 10, 32), 500, "leech", 1, "none"))  # dark red ball
+    #players.append(Player(500, 500, 80, 500, 100, 5, (125, 125, 125), 800, "mage", 1, "none")) # - Special ball
+
+def mainMenu():
+    mainText = bigFont.render("Ball fighting simulator", True, WHITE)
+    PlayText = Font.render("Play", True, WHITE)
+    SettingText = Font.render("Settings", True, WHITE)
+    ExitText = Font.render("Exit", True, WHITE)
+
+    screen.blit(mainText, ((SCREEN_WIDTH/3.5), (SCREEN_HEIGHT/5)))
+
+    draw_button(middleButtonRect, PlayText, Green, LGreen)
+    draw_button(downButtonRect, SettingText, Black, Black)
+    draw_button(bottomRect, ExitText, Black, Black)
+
+def Settings():
+    mainText = bigFont.render("Settings", True, WHITE)
+    reslText = Font.render("Resolution :", True, WHITE)
+    optText = Font.render(str(SCREEN_WIDTH) + "x" + str(SCREEN_HEIGHT), True, Black)
+
+    screen.blit(mainText, ((SCREEN_WIDTH/2.5), (SCREEN_HEIGHT/6)))
+    screen.blit(reslText, ((SCREEN_WIDTH/4), (SCREEN_HEIGHT/3)))
+
+    draw_button(optionRECT, optText, WHITE, (125, 125, 125))
+
+    if (resolutionPanel == True):
+        resl_Buttons = []
+        for index, res in enumerate(modes):
+            if (res == screen.get_size()):
+                continue
+
+            optText_temp = Font.render(str(res[0]) + "x" + str(res[1]), True, Black)
+            optRect_temp = pygame.Rect((SCREEN_WIDTH/2.4), (SCREEN_HEIGHT/3.1) + (50 * (index + 1)), 170 * (SCREENX), 50 * (SCREENX))
+                
+            draw_button(optRect_temp, optText_temp, WHITE, (125, 125, 125))
+
+            resl_Buttons.append([optRect_temp, index]) 
+
+def Game():
+    global gameTick
+    global FPS
+    gameTick += dt
+    if (gameTick >= 1):
+        FPS = math.floor(1 / dt)
+        gameTick = 0
+
+    screen.blit(tinyFont.render(str(FPS) + " FPS", True, WHITE), (0, 0))
+
+    for plr in players:
+        plr.move(dt)
+        plr.draw()
+
+        if (plr.collision_cooldown > 0):
+            plr.collision_cooldown -= dt
+
+        Combat.effectDoT(plr, dt)
+
+        if (plr.hp <= 0):
+            players.remove(plr)
+
+        for index, DoT in enumerate(plr.DoTs):
+            scaleX = (plr.r/2) * (SCREENX)
+            scaleY = (plr.r/2) * (SCREENY)
+
+            posY = (plr.y - (plr.r + 10) * SCREENY) - scaleY
+            posX = plr.x + ((index - 1) * scaleX)
+
+            if DoT == "fire":
+                screen.blit(pygame.transform.scale(fire_Image, (scaleX, scaleY)), (posX, posY))
+            if DoT == "bfire":
+                screen.blit(pygame.transform.scale(bfire_Image, (scaleX, scaleY)), (posX, posY))
+            if DoT == "poison":
+                screen.blit(pygame.transform.scale(poison_Image, (scaleX, scaleY)), (posX, posY))
+            if DoT == "static":
+                screen.blit(pygame.transform.scale(static_Image, (scaleX, scaleY)), (posX, posY))
+            if DoT == "virus":
+                screen.blit(pygame.transform.scale(virus_Image, (scaleX, scaleY)), (posX, posY))
+            if DoT == "frost":
+                screen.blit(pygame.transform.scale(frost_Image, (scaleX, scaleY)), (posX, posY))
+            if DoT == "leech":
+                screen.blit(pygame.transform.scale(leech_Image, (scaleX, scaleY)), (posX, posY))
+
+            screen.blit(tinyFont.render(str(plr.DoTs[DoT]["tier"]), True, WHITE), (posX, posY))
+            screen.blit(tinyFont.render(str(plr.DoTs[DoT]["dura"]), True, WHITE), (posX, (plr.y - (plr.r/2 + 15) * SCREENY) - scaleY))
+    if (Frame % 3 == 0) :
+        for i in range(len(players)):
+            for j in range(i+1, len(players)):
+                p1 = players[i]
+                p2 = players[j]
+
+                if Physics.CheckCollision(p1.x, p1.y, p2.x, p2.y, p1.r, p2.r):
+                    Physics.ResolveCollision(p1, p2, dt)
 
 state = "menu"
 resolutionPanel = False
 
 while running == True:
+    Frame += 1
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
         if (tick > 0.01):
@@ -163,7 +259,9 @@ while running == True:
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if (middleButtonRect.collidepoint(event.pos) and state == "menu"):
                     state = "game"
-                    initiateBattle()
+
+                    for i in range(1, 10):
+                        initiateBattle()
                 if (downButtonRect.collidepoint(event.pos) and state == "menu"):
                     state = "settings"
                 if (optionRECT.collidepoint(event.pos) and state == "settings"):
@@ -209,93 +307,14 @@ while running == True:
     screen.fill(Black)
 
     if (state == "game"):
-        if (dt > 0):
-            screen.blit(tinyFont.render(str(math.floor(1 / dt)) + " FPS", True, WHITE), (0, 0))
-        for plr in players:
-            plr.move(dt)
-            plr.draw()
-
-            if (plr.collision_cooldown > 0):
-                plr.collision_cooldown -= dt
-
-            Combat.effectDoT(plr, dt)
-
-            if (plr.hp <= 0):
-                players.remove(plr)
-
-            for index, DoT in enumerate(plr.DoTs):
-                scaleX = (plr.r/2) * (SCREENX)
-                scaleY = (plr.r/2) * (SCREENY)
-
-                posY = (plr.y - 70 * SCREENY) - scaleY
-                posX = plr.x + ((index - 1) * scaleX)
-
-                if DoT == "fire":
-                    screen.blit(pygame.transform.scale(fire_Image, (scaleX, scaleY)), (posX, posY))
-                if DoT == "bfire":
-                    screen.blit(pygame.transform.scale(bfire_Image, (scaleX, scaleY)), (posX, posY))
-                if DoT == "poison":
-                    screen.blit(pygame.transform.scale(poison_Image, (scaleX, scaleY)), (posX, posY))
-                if DoT == "static":
-                    screen.blit(pygame.transform.scale(static_Image, (scaleX, scaleY)), (posX, posY))
-                if DoT == "virus":
-                    screen.blit(pygame.transform.scale(virus_Image, (scaleX, scaleY)), (posX, posY))
-                if DoT == "frost":
-                    screen.blit(pygame.transform.scale(frost_Image, (scaleX, scaleY)), (posX, posY))
-                if DoT == "leech":
-                    screen.blit(pygame.transform.scale(leech_Image, (scaleX, scaleY)), (posX, posY))
-
-                screen.blit(tinyFont.render(str(plr.DoTs[DoT]["tier"]), True, WHITE), (posX, posY))
-                screen.blit(tinyFont.render(str(plr.DoTs[DoT]["dura"]), True, WHITE), (posX, (plr.y - 30 * SCREENY) - scaleY))
-
-        for i in range(len(players)):
-            for j in range(i+1, len(players)):
-                p1 = players[i]
-                p2 = players[j]
-
-                if Physics.CheckCollision(p1.x, p1.y, p2.x, p2.y, p1.r, p2.r):
-                    Physics.ResolveCollision(p1, p2, dt)
+       Game()
     elif (state == "menu"):
-        mainText = bigFont.render("Ball fighting simulator", True, WHITE)
-        PlayText = Font.render("Play", True, WHITE)
-        SettingText = Font.render("Settings", True, WHITE)
-        ExitText = Font.render("Exit", True, WHITE)
-
-        screen.blit(mainText, ((SCREEN_WIDTH/3.5), (SCREEN_HEIGHT/5)))
-
-        draw_button(middleButtonRect, PlayText, Green, LGreen)
-        draw_button(downButtonRect, SettingText, Black, Black)
-        draw_button(bottomRect, ExitText, Black, Black)
-    elif (state == "setup"):
-        mainText = bigFont.render("Set up your balls!", True, WHITE)
-        
-
-        screen.blit(mainText, ((SCREEN_WIDTH/3), (SCREEN_HEIGHT/4)))
+       mainMenu()
     elif (state == "settings"):
-        mainText = bigFont.render("Settings", True, WHITE)
-        reslText = Font.render("Resolution :", True, WHITE)
-        optText = Font.render(str(SCREEN_WIDTH) + "x" + str(SCREEN_HEIGHT), True, Black)
-
-        screen.blit(mainText, ((SCREEN_WIDTH/2.5), (SCREEN_HEIGHT/6)))
-        screen.blit(reslText, ((SCREEN_WIDTH/4), (SCREEN_HEIGHT/3)))
-
-        draw_button(optionRECT, optText, WHITE, (125, 125, 125))
-
-        if (resolutionPanel == True):
-            resl_Buttons = []
-            for index, res in enumerate(modes):
-                if (res == screen.get_size()):
-                    continue
-
-                optText_temp = Font.render(str(res[0]) + "x" + str(res[1]), True, Black)
-                optRect_temp = pygame.Rect((SCREEN_WIDTH/2.4), (SCREEN_HEIGHT/3.1) + (50 * (index + 1)), 170 * (SCREENX), 50 * (SCREENX))
-                
-                draw_button(optRect_temp, optText_temp, WHITE, (125, 125, 125))
-
-                resl_Buttons.append([optRect_temp, index])                    
+       Settings()                   
         
     pygame.display.flip()
-    dt = clock.tick(144) / 1000
+    dt = clock.tick(500) / 1000
     tick += dt
 
 pygame.quit()
